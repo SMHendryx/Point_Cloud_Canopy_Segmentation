6071ad3d4fecee080e005c519df20a65f0b03fec
Sean Hendryx <seanmhendryx@gmail.com>
2017-06-12 20:18:18 -0700

    Added scriptto clip raster by polygon shapefile

--- raster/clipRaster.R
+++ raster/clipRaster.R
@@ -1,1 +1,15 @@
-clipRaster.R
+library(raster)
+
+setwd("/Users/seanhendryx/DATA/Lidar/SRER/maxLeafAreaOctober2015/FUSION_rLiDAR_Output")
+
+studyArea = shapefile("/Users/seanhendryx/DATA/Lidar/SRER/Rectangular_Study_Area_UTM_EPSG26912.shp")
+
+inRaster = raster("CanopyHeightModelTLidarSRERMesTowerOct2015.tif")
+
+plot(inRaster)
+plot(studyArea, add = TRUE)
+
+outRaster = mask(inRaster, studyArea)
+plot(outRaster)
+
+writeRaster(outRaster, "rectangular_study_area/CanopyHeightModelTLidarSRERMesTowerOct2015.tif")
